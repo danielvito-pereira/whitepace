5416a683ac8b83ca65b68492da8eb705edfc5082
danielvito-pereira <danielvitor081@gmail.com>
2022-12-29 12:05:29 -0300

    ajustes na cor de fundo dos cards dos clientes

--- src/components/TestimonialSection.vue
+++ src/components/TestimonialSection.vue
@@ -21,28 +21,28 @@
       </div>
       <div class="client -blue" id="c-desk" >
         <div class="comment">
-                <img src="../assets/img/Quote_blue.png" alt="quote" srcset="">
-                <p>Whitepate is designed as a collaboration tool for businesses that is a full project management solution.</p>
+          <img src="../assets/img/Quote.png" alt="quote" srcset="">
+          <p>Whitepate is designed as a collaboration tool for businesses that is a full project management solution.</p>
         </div>
         <div class="name-box">
-                <img class="avatar" src="../assets/img/Avater2.png" alt="avatar" srcset="">
-                <div class="name">
-                  <h1>Oberon Shaw, MCH</h1>
-                  <p>Head of Talent Acquisition, North America</p>
-                </div>
+          <img class="avatar" src="../assets/img/Avater2.png" alt="avatar" srcset="">
+          <div class="name">
+            <h1>Oberon Shaw, MCH</h1>
+            <p>Head of Talent Acquisition, North America</p>
+          </div>
         </div>
       </div>
       <div class="client -blue" id="c-desk">
         <div class="comment">
-                <img src="../assets/img/Quote_blue.png" alt="quote" srcset="">
-                <p>Whitepate is designed as a collaboration tool for businesses that is a full project management solution.</p>
+          <img src="../assets/img/Quote.png" alt="quote" srcset="">
+          <p>Whitepate is designed as a collaboration tool for businesses that is a full project management solution.</p>
         </div>
         <div class="name-box">
-                <img class="avatar" src="../assets/img/Avater3.png" alt="avatar" srcset="">
-                <div class="name">
-                  <h1>Oberon Shaw, MCH</h1>
-                  <p>Head of Talent Acquisition, North America</p>
-                </div>
+          <img class="avatar" src="../assets/img/Avater3.png" alt="avatar" srcset="">
+          <div class="name">
+            <h1>Oberon Shaw, MCH</h1>
+            <p>Head of Talent Acquisition, North America</p>
+          </div>
         </div>
       </div>
     </div>
@@ -138,7 +138,7 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-    .TestimonialSection{
+  .TestimonialSection{
       display: flex;
       flex-direction: column;
       justify-content: center;
@@ -194,249 +194,163 @@ export default {
         gap: 0;
         align-items: center;
         justify-items: center;
-          .client{
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            align-items: flex-start;
-            gap: 60px;
-            width:90%;
-            padding: 1em;
-            box-shadow: 0px 4px 50px rgba(0, 0, 0, 0.08);
-            border-radius: 10px;
-            background: var(--white);
-            color: var(--paragraph);
-            min-width: 95px;
-            max-width: 471.67px;
-            min-height: 95px;
-            max-height: 498.46px;
-            .comment{
-              box-sizing: border-box;
-              display: flex;
-              flex-direction: column;
-              align-items: flex-start;
-              padding: 0px 0px 40px;
-              gap: 32px;
-              width:90%;
-              height: 192.02px;
-              border-bottom: 1px solid #212529;
-              img{
-                width: 56px;
-                height: 40.02px;
-              }
-              p{
-                font-family: 'Inter';
-                font-style: normal;
-                font-weight: 400;
-                font-size: 16px;
-                line-height: 20px;
-                letter-spacing: -0.02em;
-              }
-            }
-            .name-box{
-              display: flex;
-              flex-direction:row;
-              justify-content: flex-end;
-              align-items: flex-start;
-              gap: 30px;
-              .avatar{
-                width: 65px;
-                height: 65px;
-              }
-              .name{
-                display: flex;
-                flex-direction: column;
-                gap: 15px;
-                font-family: 'Inter';
-                font-style: normal;
-                font-weight: 700;
-                font-size: 18px;
-                line-height: 22px;
-                letter-spacing: -0.02em;
-                color: var(--paragraph);
-                h1{
-                  color: #212529;
-                }
-                
-              }
-            }
-          }
-        }
-      .priceBoard.-dourado{
-        background-color: var(--white);
-        color: var(--negro);
-        border: 1px solid var(--button2);
-        padding:0;
-        min-height: 700px;
-        gap: 0;
-        padding: 1em;
       }
-  
-      .indicadores{
+  }
+  .client{
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: flex-start;
+      gap: 60px;
+      width:90%;
+      padding: 1rem;
+      box-shadow: 0px 4px 50px rgba(0, 0, 0, 0.08);
+      border-radius: 10px;
+      background: var(--white);
+      color: var(--paragraph);
+      min-width: 5.9375rem;
+      max-width: 29.4794rem;
+      min-height: 5.9375rem;
+      max-height: 35.1537rem;
+      .comment{
+        box-sizing: border-box;
         display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: center;
-        padding: 0px;
-        gap: 12px;
-        width: 60px;
-        height: .5em;
-        
-        #bb{
-          width: 12px;
-          height: 12px;
-          border-radius:50%;
-          background: var(--blue-2);
-          cursor: pointer;
-          
-          &:hover{
-            background:var(--primary-color);
-            transition: all .5s ease-in-out;
-          }
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 0px 0px 40px;
+        gap: 32px;
+        width:90%;
+        height: 12.0013rem;
+        border-bottom: 1px solid #212529;
+        img{
+          width: 56px;
+          height: 40.02px;
         }
-    }  
-    }
-    .client{
+          p{
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 20px;
+            letter-spacing: -0.02em;
+          }
+      }
+      .name-box{
+        display: flex;
+        flex-direction:row;
+        justify-content: flex-end;
+        align-items: flex-start;
+        gap: 30px;
+        .avatar{
+          width: 65px;
+          height: 65px;
+        }
+          .name{
             display: flex;
             flex-direction: column;
-            justify-content: space-between;
-            align-items: flex-start;
-            gap: 60px;
-            width:90%;
-            padding: 1em;
-            box-shadow: 0px 4px 50px rgba(0, 0, 0, 0.08);
-            border-radius: 10px;
-            background: var(--white);
+            gap: 15px;
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 18px;
+            line-height: 22px;
+            letter-spacing: -0.02em;
             color: var(--paragraph);
-            min-width: 95px;
-            max-width: 471.67px;
-            min-height: 95px;
-            max-height: 498.46px;
-            .comment{
-              box-sizing: border-box;
-              display: flex;
-              flex-direction: column;
-              align-items: flex-start;
-              padding: 0px 0px 40px;
-              gap: 32px;
-              width:90%;
-              height: 192.02px;
-              border-bottom: 1px solid #212529;
-              img{
-                width: 56px;
-                height: 40.02px;
-              }
-              p{
-                font-family: 'Inter';
-                font-style: normal;
-                font-weight: 400;
-                font-size: 16px;
-                line-height: 20px;
-                letter-spacing: -0.02em;
-              }
+            h1{
+              color: #212529;
             }
-            .name-box{
-              display: flex;
-              flex-direction:row;
-              justify-content: flex-end;
-              align-items: flex-start;
-              gap: 30px;
-              .avatar{
-                width: 65px;
-                height: 65px;
-              }
-              .name{
-                display: flex;
-                flex-direction: column;
-                gap: 15px;
-                font-family: 'Inter';
-                font-style: normal;
-                font-weight: 700;
-                font-size: 18px;
-                line-height: 22px;
-                letter-spacing: -0.02em;
-                color: var(--paragraph);
-                h1{
-                  color: #212529;
-                }
                 
-              }
-            }
           }
-    /*swiper */
-    @media screen and (min-width: 912px){
+      }
+  }
+  .-blue{
+      background-color:var(--blue-2);
+      color: var(--white);
+      .name{
+        h1{
+          color: var(--primary-color);
+        }
+        p{
+          color: var(--white);
+        }
+      }
+  }
+  /*swiper */
+  @media screen and (max-width: 912px){
       .TestimonialSection{
         .priceList{
         display: none;
       }
-
+      .ocultar{
+        display:block;
+      } 
       }
-    }
+  }
+  .ocultar{
+    display:none;
+  } 
   .swiper-wrapper{
     padding: 1rem;
     background-color: #043873;
-    
   }
   .swiper-pagination{
     padding: 2rem;
   }
-    .swiper{
-      padding-right: 0;
-      padding: 1rem;
-
-      height:  45.25rem;
-      max-height: 75.25rem;
-
-    }
-    .priceBoard{
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: flex-start;
-      gap: 0px;
-      padding: 1em;
-      box-shadow: 0px 4px 50px rgba(0, 0, 0, 0.08);
-      border-radius: 10px;
-      background: var(--primary-color);
-      color: var(--white);
-      max-height: 853px;
-      min-height:39.875rem;
-      max-width:29.5206rem;
-    }
-    .priceBoard.-dourado{
-        background-color: var(--white);
-        color: var(--negro);
-        border: 1px solid var(--button2);
-        padding:0;
-        min-height: 39.875rem;
-        gap: 0;
-        padding: 1em;
-      }
-    .text{
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      padding: 0px;
-      gap: 15px;
-      width: 90%;
-      padding: 1em;
-    }
-    h1{
-      font-family: 'Inter';
-      font-style: normal;
-      font-weight: 600;
-      font-size: 24px;
-      line-height: 36px;
-    }
-    .point{
-      display: flex;
-      justify-content: flex-start;
-      align-items: center;
-      padding: 0px;
-      gap: 19px;
-      padding: 1em;
-      width: 100%;
-    }
+  .swiper{
+    padding-right: 0;
+    padding: 1rem;
+    height:  45.25rem;
+    max-height: 75.25rem;
+  }
+  .priceBoard{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 0px;
+    padding: 1em;
+    box-shadow: 0px 4px 50px rgba(0, 0, 0, 0.08);
+    border-radius: 10px;
+    background: var(--primary-color);
+    color: var(--white);
+    max-height: 853px;
+    min-height:39.875rem;
+    max-width:29.5206rem;
+  }
+  .priceBoard.-dourado{
+    background-color: var(--white);
+    color: var(--negro);
+    border: 1px solid var(--button2);
+    padding:0;
+    min-height: 39.875rem;
+    gap: 0;
+    padding: 1em;
+  }
+  .text{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px;
+    gap: 15px;
+    width: 90%;
+    padding: 1em;
+  }
+  h1{
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 36px;
+  }
+  .point{
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 0px;
+    gap: 19px;
+    padding: 1em;
+    width: 100%;
+  }
 
 </style>
 
